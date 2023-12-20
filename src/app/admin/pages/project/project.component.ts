eab05fb8d4c5ebcda9cf851e6d0e0a16059b5950
Jason <jason@zgwit.com>
2023-12-21 02:21:35 +0800

    优化

--- src/app/admin/pages/project/project.component.ts
+++ src/app/admin/pages/project/project.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ToolbarComponent} from "../../toolbar/toolbar.component";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [
+    ToolbarComponent,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule
+  ],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
+
+  cols = 3
+
+  constructor(bpo: BreakpointObserver) {
+    bpo.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ]).subscribe(res => {
+      console.log("bpo", res)
+      if (res.matches) {
+        if (res.breakpoints[Breakpoints.XSmall])
+          this.cols = 1
+        if (res.breakpoints[Breakpoints.Small])
+          this.cols = 2
+        if (res.breakpoints[Breakpoints.Medium])
+          this.cols = 3
+        if (res.breakpoints[Breakpoints.Large])
+          this.cols = 4
+        if (res.breakpoints[Breakpoints.XLarge])
+          this.cols = 5
+      }
+    })
+  }
 
 }
---
+++ src/app/admin/toolbar/toolbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+
+}
