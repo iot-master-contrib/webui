f9f1ebc661bbbd48586d595138af51b633db60d4
Jason <jason@zgwit.com>
2023-12-22 00:54:20 +0800

    更新改 ng-zorro（Material不习惯呀）

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,39 +1,16 @@
-import {ApplicationConfig, LOCALE_ID} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { zh_CN, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import {MatPaginatorIntl} from "@angular/material/paginator";
+
+registerLocaleData(zh);
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideAnimations(),
-    {provide: LOCALE_ID, useValue: "zh-CN"},
-    { provide: MatPaginatorIntl, useValue: myPaginator() },
-  ]
+  providers: [provideRouter(routes), provideNzI18n(zh_CN), importProvidersFrom(FormsModule), importProvidersFrom(HttpClientModule), provideAnimations()]
 };
-
-function dutchRangeLabel(page: number, pageSize: number, length: number) {
-  if (length === 0 || pageSize === 0) {
-    return `0 到 ${length}`;
-  }
-  length = Math.max(length, 0);
-  const startIndex = page * pageSize;
-  const endIndex = startIndex < length ?
-    Math.min(startIndex + pageSize, length) :
-    startIndex + pageSize;
-  return `${startIndex + 1} - ${endIndex} 条/总共 ${length}条`;
-};
-
-export function myPaginator(): any {
-  const paginatorIntl = new MatPaginatorIntl();
-  paginatorIntl.itemsPerPageLabel = '每页数据条数:';
-  paginatorIntl.nextPageLabel = '下一页';
-  paginatorIntl.previousPageLabel = '上一页';
-  paginatorIntl.firstPageLabel = '首页';
-  paginatorIntl.lastPageLabel = '尾页';
-  paginatorIntl.getRangeLabel = dutchRangeLabel;
-
-  return paginatorIntl;
-}
