5b2c620ad0569fc01899015333958e969f5b88b1
Jason <jason@zgwit.com>
2023-12-27 12:14:18 +0800

    设置页面

--- src/app/admin/pages/setting/setting.component.ts
+++ src/app/admin/pages/setting/setting.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {NzLayoutComponent, NzLayoutModule} from "ng-zorro-antd/layout";
+import {NzMenuDirective, NzMenuItemComponent, NzSubMenuComponent} from "ng-zorro-antd/menu";
+import {NzIconDirective} from "ng-zorro-antd/icon";
+import {NzInputDirective} from "ng-zorro-antd/input";
 
 @Component({
   selector: 'app-setting',
   standalone: true,
-  imports: [],
+  imports: [
+    NzLayoutModule,
+    NzMenuDirective,
+    NzMenuItemComponent,
+    NzIconDirective,
+    NzSubMenuComponent,
+    NzInputDirective
+  ],
   templateUrl: './setting.component.html',
   styleUrl: './setting.component.scss'
 })
