a493af9766c624838fa921c600be2c801d9bcd13
Jason <jason@zgwit.com>
2023-12-27 08:32:20 +0800

    项目编辑页面

--- src/app/admin/pages/project-edit/project-edit.component.ts
+++ src/app/admin/pages/project-edit/project-edit.component.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {DatePipe} from "@angular/common";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzDescriptionsComponent, NzDescriptionsItemComponent} from "ng-zorro-antd/descriptions";
+import {
+  NzPageHeaderComponent,
+  NzPageHeaderContentDirective,
+  NzPageHeaderExtraDirective, NzPageHeaderSubtitleDirective, NzPageHeaderTitleDirective
+} from "ng-zorro-antd/page-header";
+import {NzPopconfirmDirective} from "ng-zorro-antd/popconfirm";
+import {NzSpaceComponent, NzSpaceItemDirective} from "ng-zorro-antd/space";
+import {RouterLink} from "@angular/router";
+import {NzFormDirective, NzFormItemComponent, NzFormModule} from "ng-zorro-antd/form";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NzInputDirective, NzTextareaCountComponent} from "ng-zorro-antd/input";
+import {NzUploadChangeParam, NzUploadComponent} from "ng-zorro-antd/upload";
+import {NzIconDirective} from "ng-zorro-antd/icon";
+import {NzSelectComponent} from "ng-zorro-antd/select";
 
 @Component({
   selector: 'app-project-edit',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe,
+    NzButtonComponent,
+    NzDescriptionsComponent,
+    NzDescriptionsItemComponent,
+    NzPageHeaderComponent,
+    NzPageHeaderContentDirective,
+    NzPageHeaderExtraDirective,
+    NzPageHeaderSubtitleDirective,
+    NzPageHeaderTitleDirective,
+    NzPopconfirmDirective,
+    NzSpaceComponent,
+    RouterLink,
+    NzSpaceItemDirective,
+    FormsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputDirective,
+    NzTextareaCountComponent,
+    NzUploadComponent,
+    NzIconDirective,
+    NzSelectComponent,
+  ],
   templateUrl: './project-edit.component.html',
   styleUrl: './project-edit.component.scss'
 })
 export class ProjectEditComponent {
+  data: any = {
+    name: "测试项目",
+  }
+  formGroup!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.buildFromGroup()
+  }
+
+  buildFromGroup(){
+    this.formGroup = this.fb.group({
+      name: [this.data.name || '', []],
+      description: [this.data.description || '', []],
+      icon: [this.data.icon || '', []],
+      version: [this.data.version || '', []],
+      url: [this.data.url || '', []],
+      keywords: [this.data.keywords || [], []],
+    })
+  }
+
+  onSubmit() {
+
+  }
+
+
+  onIconChange($event: NzUploadChangeParam) {
+
+  }
+
 
 }
