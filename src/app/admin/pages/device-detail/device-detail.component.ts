1c88f8aeb2f8e059ebbceaaad2daf84d3801c975
Jason <jason@zgwit.com>
2023-12-27 09:00:06 +0800

    设备和产品详情

--- src/app/admin/pages/device-detail/device-detail.component.ts
+++ src/app/admin/pages/device-detail/device-detail.component.ts
@@ -1,12 +1,79 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NzPageHeaderComponent, NzPageHeaderModule} from "ng-zorro-antd/page-header";
+import {NzSpaceModule} from "ng-zorro-antd/space";
+import {NzDescriptionsModule} from "ng-zorro-antd/descriptions";
+import {PluginsComponent} from "../../plugins/plugins.component";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzIconDirective} from "ng-zorro-antd/icon";
+import {NzInputDirective, NzInputGroupComponent} from "ng-zorro-antd/input";
+import {DatePipe, NgForOf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {NzPopconfirmDirective} from "ng-zorro-antd/popconfirm";
+import {NzStatisticComponent} from "ng-zorro-antd/statistic";
+import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
 
 @Component({
   selector: 'app-device-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    NzPageHeaderModule,
+    NzDescriptionsModule,
+    NzSpaceModule,
+    PluginsComponent,
+    NzButtonComponent,
+    NzIconDirective,
+    NzInputGroupComponent,
+    NzInputDirective,
+    NgForOf,
+    RouterLink,
+    NzPopconfirmDirective,
+    DatePipe,
+    NzStatisticComponent,
+    NzRowDirective,
+    NzColDirective,
+  ],
   templateUrl: './device-detail.component.html',
   styleUrl: './device-detail.component.scss'
 })
 export class DeviceDetailComponent {
+  data: any = {
+    id: 1,
+    name: '温度计',
+    description: '标准485温度计',
+    version: '3',
+    keywords: ['modbus'],
+    icon: '',
+    url: 'https://www.baidu.com',
+    created: new Date(),
+    properties: [
+      {name: "temp", label: "温度", unit: '℃'},
+      {name: "hum", label: "湿度", unit: '%'},
+    ]
+  }
+
+  variables: any = {
+    temp: 30,
+    hum: 71,
+  }
+
+
+  plugins: any[] = [
+    {
+      id: 'modbus',
+      name: 'Modbus',
+      icon: 'assets/app.png',
+      description: 'Modbus映射编辑'
+    },
+    {
+      id: 'history',
+      name: '历史数据',
+      icon: 'assets/app.png',
+      description: ''
+    },
+  ];
+
+  delete() {
+
+  }
 
 }
--- src/app/admin/pages/product-detail/product-detail.component.ts
+++ src/app/admin/pages/product-detail/product-detail.component.ts
@@ -1,15 +1,64 @@
-import { Component } from '@angular/core';
-import {NzPageHeaderComponent} from "ng-zorro-antd/page-header";
+import {Component} from '@angular/core';
+import {NzPageHeaderComponent, NzPageHeaderModule} from "ng-zorro-antd/page-header";
+import {NzSpaceModule} from "ng-zorro-antd/space";
+import {NzDescriptionsModule} from "ng-zorro-antd/descriptions";
+import {PluginsComponent} from "../../plugins/plugins.component";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzIconDirective} from "ng-zorro-antd/icon";
+import {NzInputDirective, NzInputGroupComponent} from "ng-zorro-antd/input";
+import {DatePipe, NgForOf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+import {NzPopconfirmDirective} from "ng-zorro-antd/popconfirm";
+import {NzStatisticComponent} from "ng-zorro-antd/statistic";
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
   imports: [
-    NzPageHeaderComponent
+    NzPageHeaderModule,
+    NzDescriptionsModule,
+    NzSpaceModule,
+    PluginsComponent,
+    NzButtonComponent,
+    NzIconDirective,
+    NzInputGroupComponent,
+    NzInputDirective,
+    NgForOf,
+    RouterLink,
+    NzPopconfirmDirective,
+    DatePipe,
+    NzStatisticComponent,
   ],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
 export class ProductDetailComponent {
+  data: any = {
+    id: 1,
+    name: '温度计',
+    description: '标准485温度计',
+    version: '3',
+    keywords: ['modbus'],
+    icon: '',
+    url: 'https://www.baidu.com',
+    created: new Date(),
+    properties: [
+      {name: "temp", label: "温度", unit: '℃'},
+      {name: "hum", label: "湿度", unit: '%'},
+    ]
+  }
+
+  plugins: any[] = [
+    {
+      id: 'modbus',
+      name: 'Modbus',
+      icon: 'assets/app.png',
+      description: 'Modbus映射编辑'
+    },
+  ];
+
+  delete() {
+
+  }
 
 }
