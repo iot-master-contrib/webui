12e103bdde14154c710e79364418385176506174
Jason <jason@zgwit.com>
2023-12-30 03:04:37 +0800

    添加日志配置页面

--- src/app/admin/pages/setting-broker/setting-broker.component.ts
+++ src/app/admin/pages/setting-broker/setting-broker.component.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
+import {NzFormControlComponent, NzFormDirective, NzFormItemComponent, NzFormLabelComponent} from "ng-zorro-antd/form";
+import {NzInputDirective} from "ng-zorro-antd/input";
+import {NzOptionComponent, NzSelectComponent} from "ng-zorro-antd/select";
+import {
+    NzPageHeaderComponent,
+    NzPageHeaderExtraDirective,
+    NzPageHeaderSubtitleDirective, NzPageHeaderTitleDirective
+} from "ng-zorro-antd/page-header";
+import {NzSpaceComponent, NzSpaceItemDirective} from "ng-zorro-antd/space";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-setting-broker',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NzButtonComponent,
+    NzColDirective,
+    NzFormControlComponent,
+    NzFormDirective,
+    NzFormItemComponent,
+    NzFormLabelComponent,
+    NzInputDirective,
+    NzOptionComponent,
+    NzPageHeaderComponent,
+    NzPageHeaderExtraDirective,
+    NzPageHeaderSubtitleDirective,
+    NzPageHeaderTitleDirective,
+    NzRowDirective,
+    NzSelectComponent,
+    NzSpaceComponent,
+    ReactiveFormsModule,
+    NzSpaceItemDirective,
+    NgIf
+  ],
   templateUrl: './setting-broker.component.html',
   styleUrl: './setting-broker.component.scss'
 })
 export class SettingBrokerComponent {
+  formGroup!: FormGroup;
+
+  data: any = {}
+
+  constructor(private fb: FormBuilder) {
+    this.buildFromGroup()
+  }
+
+  buildFromGroup() {
+    this.formGroup = this.fb.group({
+      type: [this.data.type || 'internal', []],
+      address: [this.data.address || 'localhost', []],
+      port: [this.data.port || 1883, []],
+      username: [this.data.username || '', []],
+      password: [this.data.password || '', []],
+      client_id: [this.data.client_id || 'master', []],
+    })
+    //console.log(this.formGroup)
+  }
+
+  onSubmit() {
+
+  }
 
 }
--- src/app/admin/pages/setting-log/setting-log.component.ts
+++ src/app/admin/pages/setting-log/setting-log.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
+import {NzFormControlComponent, NzFormDirective, NzFormItemComponent, NzFormLabelComponent} from "ng-zorro-antd/form";
+import {NzInputDirective} from "ng-zorro-antd/input";
+import {NzOptionComponent, NzSelectComponent} from "ng-zorro-antd/select";
+import {
+  NzPageHeaderComponent,
+  NzPageHeaderExtraDirective,
+  NzPageHeaderSubtitleDirective, NzPageHeaderTitleDirective
+} from "ng-zorro-antd/page-header";
+import {NzSpaceComponent} from "ng-zorro-antd/space";
+import {NzSwitchComponent} from "ng-zorro-antd/switch";
 
 @Component({
   selector: 'app-setting-log',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf,
+    NzButtonComponent,
+    NzColDirective,
+    NzFormControlComponent,
+    NzFormDirective,
+    NzFormItemComponent,
+    NzFormLabelComponent,
+    NzInputDirective,
+    NzOptionComponent,
+    NzPageHeaderComponent,
+    NzPageHeaderExtraDirective,
+    NzPageHeaderSubtitleDirective,
+    NzPageHeaderTitleDirective,
+    NzRowDirective,
+    NzSelectComponent,
+    NzSpaceComponent,
+    ReactiveFormsModule,
+    NzSwitchComponent
+  ],
   templateUrl: './setting-log.component.html',
   styleUrl: './setting-log.component.scss'
 })
 export class SettingLogComponent {
+  formGroup!: FormGroup;
 
+  data: any = {}
+
+  constructor(private fb: FormBuilder) {
+    this.buildFromGroup()
+  }
+
+  buildFromGroup() {
+    this.formGroup = this.fb.group({
+      level: [this.data.level || 'info', []],
+      caller: [this.data.caller || true, []],
+      text: [this.data.text || true, []],
+      filename: [this.data.filename || "", []],
+      max_size: [this.data.max_size || 10, []],  //MB
+      max_backups: [this.data.max_backups || 3, []], //保留文件数
+      max_age: [this.data.max_age || 30, []],   //天
+      compress: [this.data.compress || true, []], //gzip压缩
+    })
+    //console.log(this.formGroup)
+  }
+
+  onSubmit() {
+
+  }
 }
