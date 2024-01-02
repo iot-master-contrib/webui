85bf263626c0e07a4b2df55471aaed9390427bed
Jason <jason@zgwit.com>
2024-01-03 02:16:31 +0800

    弹窗

---
+++ src/app/admin/modals/create/create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.scss'
+})
+export class CreateComponent {
+
+}
---
+++ src/app/admin/modals/object-attach/object-attach.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-object-attach',
+  standalone: true,
+  imports: [],
+  templateUrl: './object-attach.component.html',
+  styleUrl: './object-attach.component.scss'
+})
+export class ObjectAttachComponent {
+
+}
