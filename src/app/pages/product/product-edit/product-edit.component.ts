65ad8ff5f5703c761ac52f30d611bde9ab982222
Jason <jason@zgwit.com>
2024-01-23 00:25:41 +0800

    小优化

--- src/app/pages/product/product-edit/product-edit.component.ts
+++ src/app/pages/product/product-edit/product-edit.component.ts
@@ -1,7 +1,7 @@
-import { filter } from 'rxjs/operators';
-import { Component, signal, OnInit } from '@angular/core';
-import { DatePipe, CommonModule } from '@angular/common';
-import { NzButtonComponent } from 'ng-zorro-antd/button';
+import {filter} from 'rxjs/operators';
+import {Component, signal, OnInit} from '@angular/core';
+import {DatePipe, CommonModule} from '@angular/common';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
 import {
   NzDescriptionsComponent,
   NzDescriptionsItemComponent,
@@ -13,9 +13,9 @@ import {
   NzPageHeaderSubtitleDirective,
   NzPageHeaderTitleDirective,
 } from 'ng-zorro-antd/page-header';
-import { NzPopconfirmDirective } from 'ng-zorro-antd/popconfirm';
-import { NzSpaceComponent, NzSpaceItemDirective } from 'ng-zorro-antd/space';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import {NzPopconfirmDirective} from 'ng-zorro-antd/popconfirm';
+import {NzSpaceComponent, NzSpaceItemDirective} from 'ng-zorro-antd/space';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
 import {
   NzFormDirective,
   NzFormItemComponent,
@@ -31,11 +31,12 @@ import {
   NzInputDirective,
   NzTextareaCountComponent,
 } from 'ng-zorro-antd/input';
-import { NzUploadChangeParam, NzUploadComponent } from 'ng-zorro-antd/upload';
-import { NzIconDirective } from 'ng-zorro-antd/icon';
-import { NzSelectComponent } from 'ng-zorro-antd/select';
-import { RequestService } from '../../../request.service';
-import { NzMessageService } from 'ng-zorro-antd/message';
+import {NzUploadChangeParam, NzUploadComponent} from 'ng-zorro-antd/upload';
+import {NzIconDirective} from 'ng-zorro-antd/icon';
+import {NzSelectComponent} from 'ng-zorro-antd/select';
+import {RequestService} from '../../../request.service';
+import {NzMessageService} from 'ng-zorro-antd/message';
+
 @Component({
   selector: 'app-product-edit',
   standalone: true,
@@ -71,7 +72,8 @@ export class ProductEditComponent implements OnInit {
     name: '新产品',
   };
   formGroup!: FormGroup;
-  id: any = 0;
+  id: any = '';
+
   constructor(
     private fb: FormBuilder,
     private router: Router,
@@ -85,7 +87,7 @@ export class ProductEditComponent implements OnInit {
   buildFromGroup(data?: any) {
     data = data || {};
     this.formGroup = this.fb.group({
-      id: [this.id, []],
+      id: [this.id || '', []],
       name: [data.name || '', []],
       description: [data.description || '', []],
       icon: [data.icon || '', []],
@@ -102,8 +104,9 @@ export class ProductEditComponent implements OnInit {
       this.load();
     }
   }
+
   load() {
-    this.rs.post(`product/search`, { filter:{id:this.id}}).subscribe(
+    this.rs.post(`product/search`, {filter: {id: this.id}}).subscribe(
       (res) => {
         this.buildFromGroup(res.data);
       },
@@ -112,9 +115,10 @@ export class ProductEditComponent implements OnInit {
       }
     );
   }
+
   onSubmit() {
     if (this.formGroup.valid) {
-      let url =  `product/${this.id}`  ;
+      let url = `product/${this.id}`;
       this.rs.post(url, this.formGroup.value).subscribe((res) => {
         this.router.navigateByUrl('admin/product');
         this.msg.success('保存成功');
@@ -125,11 +129,12 @@ export class ProductEditComponent implements OnInit {
       Object.values(this.formGroup.controls).forEach((control) => {
         if (control.invalid) {
           control.markAsDirty();
-          control.updateValueAndValidity({ onlySelf: true });
+          control.updateValueAndValidity({onlySelf: true});
         }
       });
     }
   }
 
-  onIconChange($event: NzUploadChangeParam) {}
+  onIconChange($event: NzUploadChangeParam) {
+  }
 }
